690d9341a4f5f3bdc5691caae296f33b39f0b568
Bao <baolhq@cyno.com.vn>
2025-06-22 21:16:46 +0700

    upload hero section

---
+++ app/components/hero.tsx
@@ -0,0 +1,167 @@
+'use client'
+import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
+import { gsap } from "gsap";
+import SplitType from "split-type";
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollTrigger);
+
+
+const Hero = () => {
+    useGSAP(() => {
+        const splitTitle = new SplitType("#hero-title", { types: "chars", });
+        const splitLeftTitle = new SplitType(".hero-left-content__title", { types: "words" });
+        const tl = gsap.timeline()
+        const splitRightContent = new SplitType(".hero-right-content__content", { types: "lines" });
+
+        tl
+            .from(splitTitle.chars, {
+                y: 100,
+                opacity: 0,
+                stagger: { each: 0.1 },
+                duration: 0.8,
+                ease: "back",
+            })
+            .from(splitLeftTitle.words, {
+                x: -100,
+                opacity: 0,
+                stagger: { each: 0.2 },
+                duration: 1,
+                ease: "back.out(1.7)", // smoother back easing
+            }, "0.5")
+            .from(splitRightContent.lines, {
+                y: 200,
+                opacity: 0,
+                duration: 1,
+                stagger: { each: 0.2 },
+                ease: "power2.out",
+            }, "<")
+            .from(".hero-left-content__content", {
+                y: 200,
+                opacity: 0,
+                duration: 1,
+                ease: "power2.out",
+            }, "<")
+            .from(".hero-right-content__link", {
+                y: 200,
+                opacity: 0,
+                duration: 1,
+                ease: "power2.out",
+            }, "<");
+
+
+        const leafTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#hero",
+                start: "top-=30 top",
+                end: "bottom top",
+                scrub: true,
+                // markers: true, // ⬅️ shows scroll lines in browser
+            },
+        });
+
+        leafTl
+            .to("#left-leaf", { y: -300, duration: 1 })
+            .to("#right-leaf", { y: 300, duration: 1 }, "<"); // "<" makes them animate together
+
+        return () => {
+            splitTitle.revert();
+            splitLeftTitle.revert();
+            splitRightContent.revert();
+            tl.kill();
+        };
+    }, []);
+
+    useEffect(() => {
+        const link = document.querySelector("#cocktail-link");
+
+        const onEnter = () => {
+            gsap.to(link, {
+                x: 10,
+                duration: 0.5,
+                ease: "power2.out",
+            });
+        };
+
+        const onLeave = () => {
+            gsap.to(link, {
+                x: 0,
+                duration: 0.3,
+                ease: "bounce",
+            });
+        };
+
+        if (link) {
+            link.addEventListener("mouseenter", onEnter);
+            link.addEventListener("mouseleave", onLeave);
+        }
+
+        // Cleanup on unmount
+        return () => {
+            if (link) {
+                link.removeEventListener("mouseenter", onEnter);
+                link.removeEventListener("mouseleave", onLeave);
+            }
+        };
+    }, []);
+
+
+    return (
+        <div id="hero" className='relative overflow-hidden flex justify-center px-30'>
+            <Image
+                src="/images/noise.png"
+                alt=""
+                height={2000}
+                width={2000}
+                className="absolute w-5/6 h-full object-cover init-0 opacity-100 mix-blend-screen mask-fade-circle"
+            />
+            <div className="absolute w-5/6 h-5/6 glow-circle pointer-events-none mix-blend-screen init-0" />
+            <div className='z-1 w-full'>
+                <h1 id="hero-title" className='font-negra text-[300px] uppercase tracking-wide text-center'>
+                    Mojito
+                </h1>
+                <Image
+                    src="/images/hero-left-leaf.png"
+                    alt=""
+                    height={325}
+                    width={325}
+                    className='absolute left-0 top-20 w-[250px] object-contain z-0'
+                    id="left-leaf"
+                />
+                <Image
+                    src="/images/hero-right-leaf.png"
+                    alt=""
+                    height={325}
+                    width={325}
+                    className='absolute right-0 -top-16 w-[200px] object-contain z-0'
+                    id="right-leaf"
+                />
+                <div className='flex flex-row justify-between z-1 xl:mt-20 md:hidden xl:flex'>
+                    <div>
+                        <p className='hero-left-content__title font-light text-base'>Cool. Crisp. Classic.</p>
+                        <h2 className='hero-left-content__content font-negra text-5xl mt-2' style={{ color: 'rgba(231, 211, 147, 1)' }}>Sip the Spirit <br /> of Summer</h2>
+                    </div>
+                    <div className='hero-right-container xl:w-[300px]'>
+                        <p className='hero-right-content__content font-light mb-3 w-[300px]'>Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses. </p>
+                        <div id="cocktail-link" className='hero-right-content__link flex flex-row gap-1 items-center'>
+                            <Link href={''} className='font-light text-white opacity-80 text-xs tracking-[3px]'>View cocktails </Link>
+                            <ArrowRight size={16} color='white' className='opacity-80' />
+                        </div>
+                    </div>
+                    {/* <Image
+                        src={}
+                    /> */}
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ app/components/nav.tsx
@@ -0,0 +1,78 @@
+'use client'
+import { navLinks } from '@/data/data'
+import Image from 'next/image'
+import React, { useRef } from 'react'
+import gsap from 'gsap'
+import { SplitText } from "gsap/SplitText";
+import Link from 'next/link'
+gsap.registerPlugin(SplitText);
+
+
+const NavBar = () => {
+    const buttonRefs = useRef<(HTMLSpanElement | null)[]>([]) // store refs in an array
+    const underlineRefs = useRef<(HTMLSpanElement | null)[]>([])
+
+    const HoverButton = (index) => {
+        const underline = underlineRefs.current[index]
+        gsap.to(underline, {
+            scaleX: 1,
+            duration: 0.5,
+            ease: "power2.out",
+        })
+    }
+
+    // Optional: revert on mouse leave
+    const LeaveButton = (index) => {
+        const underline = underlineRefs.current[index]
+        gsap.to(underline, {
+            scaleX: 0,
+            duration: 0.5,
+            ease: 'power2.out',
+        })
+    }
+
+    return (
+        <div className='h-[90px] flex flex-row items-center'>
+            <div className='px-[90px] flex flex-row justify-between items-center w-full'>
+                <div className='flex gap-1 items-center relative'>
+                    <Image
+                        src={'/images/fav.png'}
+                        height={32}
+                        width={32}
+                        alt=''
+                        className='size-8'
+                    />
+                    <p className="font-negra text-[28px] absolute top-0 inline-block w-[200px] left-9">
+                        Velvet Pour
+                    </p>
+                </div>
+                <div className='flex flex-row items-center gap-12 font-light text-sm'>
+                    {navLinks.map((item, index) => {
+                        return (
+                            <div
+                                ref={el => { buttonRefs.current[index] = el }} // assign ref
+                                onMouseEnter={() => HoverButton(index)}
+                                onMouseLeave={() => LeaveButton(index)}
+                                key={index}
+                                className='relative'
+                            >
+                                <Link
+                                    href={"localhost:3000"}
+                                    className="transition-all"
+                                >
+                                    {item.title}
+                                </Link>
+                                <span
+                                    ref={el => { underlineRefs.current[index] = el }}
+                                    className="absolute left-0 bottom-[-2px] h-[2px] w-full bg-white origin-left scale-x-0"
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
